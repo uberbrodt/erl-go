9bcde6ed18b478833d4c914d8e34bbb007580e9d
Chris Brodt <chris@uberbrodt.net>
2024-11-11 13:34:08 -0600

    inbox: fix some mutexes and add some docs

--- erl/internal/inbox/inbox.go
+++ erl/internal/inbox/inbox.go
@@ -11,6 +11,15 @@ type Inbox[M any] struct {
 	closed bool
 }
 
+// Create an Inbox that will store messages of type [M].
+// An Inbox is written to using the `Enqueue` method, which will
+// append itself to the end of a message queue. To read these messages,
+// there are two methods:
+//
+//  1. Calling [Pop], which will return one message at a time.
+//  2. Call [Receive], which will return a channel that will receive a message one at a time. This
+//     method is useful if you expect to call [Pop] in a loop or if you want to have multiple consumers
+//     of the inbox and order is not important (a version of the competeing consumers pattern).
 func New[M any]() *Inbox[M] {
 	return &Inbox[M]{
 		msgQ: make([]M, 0, 10),
@@ -35,8 +44,6 @@ func (i *Inbox[M]) Enqueue(msg M) bool {
 // Returns a channel that will send values as long as the inbox is open.
 // The returned channel will be closed if [Close] is called on the inbox.
 func (i *Inbox[M]) Receive() <-chan M {
-	i.mx.Lock()
-	defer i.mx.Unlock()
 	rec := make(chan M)
 
 	go func() {
@@ -65,14 +72,13 @@ func (i *Inbox[M]) Receive() <-chan M {
 // get and remove a value from the inbox. This is safe to call from multiple go routines.
 func (i *Inbox[M]) Pop() (M, bool) {
 	var null M
-	i.mx.RLock()
-	defer i.mx.RUnlock()
+	i.mx.Lock()
+	defer i.mx.Unlock()
 	if len(i.msgQ) == 0 {
 		return null, false
 	}
 
 	head := i.msgQ[0]
-	// XXX: bounds checking?
 	tail := i.msgQ[1:]
 	i.msgQ = tail
 
