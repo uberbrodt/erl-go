1fce12d4ab04d31cce8468d88a9f49f3fe09b1e2
Chris Brodt <chris@uberbrodt.net>
2024-12-05 11:06:25 -0600

    Added synchronization options to inbox
    Added three methods:
    
    - Sync, to get the underlying sync.Cond
    - BlockingPop, which uses the sync.Cond to return values only when
      they're ready but otherwise sleep the calling goroutine
    - Iter, which uses the new go 1.23 iterators framework but works
      similarly to BlockingPop

--- erl/internal/inbox/inbox.go
+++ erl/internal/inbox/inbox.go
@@ -2,6 +2,7 @@ package inbox
 
 import (
 	"fmt"
+	"iter"
 	"sync"
 )
 
@@ -10,6 +11,7 @@ type Inbox[M any] struct {
 	mx     sync.RWMutex
 	done   chan struct{}
 	closed bool
+	cond   *sync.Cond
 }
 
 // Create an Inbox that will store messages of type [M].
@@ -22,10 +24,12 @@ type Inbox[M any] struct {
 //     method is useful if you expect to call [Pop] in a loop or if you want to have multiple consumers
 //     of the inbox and order is not important (a version of the competeing consumers pattern).
 func New[M any]() *Inbox[M] {
-	return &Inbox[M]{
+	i := &Inbox[M]{
 		msgQ: make([]M, 0, 10),
 		done: make(chan struct{}),
 	}
+	i.cond = sync.NewCond(&sync.Mutex{})
+	return i
 }
 
 // Add a message to the end of the k
@@ -34,25 +38,34 @@ func (i *Inbox[M]) Enqueue(msg M) bool {
 	defer i.mx.Unlock()
 
 	if i.closed {
+		i.cond.Broadcast()
 		return false
 	}
 
 	i.msgQ = append(i.msgQ, msg)
 
+	i.cond.Broadcast()
 	return true
 }
 
+func (i *Inbox[M]) Sync() *sync.Cond {
+	// i.mx.Lock()
+	// defer i.mx.Unlock()
+	return i.cond
+}
+
 // get and remove a value from the inbox. This is safe to call from multiple go routines.
-func (i *Inbox[M]) Pop() (M, bool, error) {
-	var null M
+// if there was no item returned, [ok] returns false
+// if the inbox is closed and will never return a value, [closed] will be not nil
+func (i *Inbox[M]) Pop() (item M, ok bool, closed error) {
 	i.mx.Lock()
 	defer i.mx.Unlock()
 
 	if i.closed {
-		return null, false, fmt.Errorf("inbox closed")
+		return item, false, fmt.Errorf("inbox closed")
 	}
 	if len(i.msgQ) == 0 {
-		return null, false, nil
+		return item, false, nil
 	}
 
 	head := i.msgQ[0]
@@ -60,6 +73,70 @@ func (i *Inbox[M]) Pop() (M, bool, error) {
 	i.msgQ = tail
 
 	return head, true, nil
+}
+
+// Similar to [Pop], but this call will block until it has a value to retrieve.
+// This is safe to call from multiple go routines, but keep in mind that [item] may
+// be empty in that case, so always ensure that you actually got a value by checking [ok].
+//
+// If the inbox is closed, [closed] will be non-nil and the caller can expect no more
+// messages.
+// Under the hood, this is using [sync.Cond] to sleep callers until there are messages.
+func (i *Inbox[M]) BlockingPop() (item M, ok bool, closed error) {
+	c := i.Sync()
+	c.L.Lock()
+	defer c.L.Unlock()
+	item, ok, closed = i.Pop()
+	if closed != nil {
+		return
+	}
+	if !ok {
+		c.Wait()
+		return i.Pop()
+	}
+	return
+}
+
+// this is an Iterator function that can be used with a range loop like so:
+//
+//	for item, ok := range ibox.Iter() {
+//		if !ok {
+//			log.Println("someone beat us to it, try again")
+//			continue
+//		}
+//		log.Printf("got value: %d", item)
+//		sig <- item
+//	}
+//
+// The loop will exit when the inbox is closed. Note that you need to check
+// [ok] still to ensure that you actually got a value, since multiple go routines
+// may be reading off this same machine.
+func (i *Inbox[M]) Iter() iter.Seq2[M, bool] {
+	return func(yield func(M, bool) bool) {
+		c := i.Sync()
+		c.L.Lock()
+		defer c.L.Unlock()
+		for {
+			item, ok, closed := i.Pop()
+			if closed != nil {
+				return
+			}
+			if !ok {
+				c.Wait()
+				item2, ok2, closed := i.Pop()
+				if closed != nil {
+					return
+				}
+				if !yield(item2, ok2) {
+					return
+				}
+
+			}
+			if !yield(item, ok) {
+				return
+			}
+		}
+	}
 }
 
 // Return the number of items in the Inbox
--- erl/internal/inbox/inbox_test.go
+++ erl/internal/inbox/inbox_test.go
@@ -55,3 +55,127 @@ func TestSize_ReturnsNumberOfItems(t *testing.T) {
 
 	assert.Equal(t, result, 3)
 }
+
+func TestSync_CanBeCalledMultipleTimes(t *testing.T) {
+	sig := make(chan int)
+	ended := make(chan struct{})
+
+	ibox := inbox.New[int]()
+
+	go func() {
+		c := ibox.Sync()
+		c.L.Lock()
+		defer c.L.Unlock()
+		t.Log("goroutine start Wait loop")
+		for {
+			v, ok, err := ibox.Pop()
+			if err != nil {
+				t.Log("inbox closed")
+				// exit go routine
+				close(ended)
+				return
+			}
+			if !ok {
+				t.Log("no values in inbox, wait for more")
+				c.Wait()
+				t.Log("Wait() unblocked")
+				continue
+			}
+			t.Logf("got value: %d", v)
+			// set value onto our sig channel
+			sig <- v
+		}
+	}()
+
+	t.Log("enqueue item 1")
+	ibox.Enqueue(1)
+	t.Log("enqueue item 2")
+	ibox.Enqueue(2)
+	t.Log("close inbox")
+
+	t.Log("waiting for item 1")
+	item1 := <-sig
+	assert.Equal(t, item1, 1)
+
+	t.Log("waiting for item 2")
+	item2 := <-sig
+	assert.Equal(t, item2, 2)
+	ibox.Close()
+	<-ended
+}
+
+func TestBlockingPop(t *testing.T) {
+	sig := make(chan int)
+	ended := make(chan struct{})
+
+	ibox := inbox.New[int]()
+
+	go func() {
+		for {
+			item, ok, closed := ibox.BlockingPop()
+			if closed != nil {
+				t.Log("inbox closed")
+				close(ended)
+				return
+
+			}
+			if !ok {
+				t.Log("someone beat us to it, try again")
+				continue
+			}
+			t.Logf("got value: %d", item)
+			sig <- item
+		}
+	}()
+
+	t.Log("enqueue item 1")
+	ibox.Enqueue(1)
+	t.Log("enqueue item 2")
+	ibox.Enqueue(2)
+	t.Log("close inbox")
+
+	t.Log("waiting for item 1")
+	item1 := <-sig
+	assert.Equal(t, item1, 1)
+
+	t.Log("waiting for item 2")
+	item2 := <-sig
+	assert.Equal(t, item2, 2)
+	ibox.Close()
+	<-ended
+}
+
+func TestIter(t *testing.T) {
+	sig := make(chan int)
+	ended := make(chan struct{})
+
+	ibox := inbox.New[int]()
+
+	go func() {
+		for item, ok := range ibox.Iter() {
+			if !ok {
+				t.Log("someone beat us to it, try again")
+				continue
+			}
+			t.Logf("got value: %d", item)
+			sig <- item
+		}
+		close(ended)
+	}()
+
+	t.Log("enqueue item 1")
+	ibox.Enqueue(1)
+	t.Log("enqueue item 2")
+	ibox.Enqueue(2)
+	t.Log("close inbox")
+
+	t.Log("waiting for item 1")
+	item1 := <-sig
+	assert.Equal(t, item1, 1)
+
+	t.Log("waiting for item 2")
+	item2 := <-sig
+	assert.Equal(t, item2, 2)
+	ibox.Close()
+	<-ended
+}
